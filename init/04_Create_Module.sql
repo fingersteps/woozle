d0c1b7f106de24f1f6033d11d07ab84e999226a6
paroos <pa.roos@icloud.com>
2013-12-27 14:10:03 +0100

    Example SQL Script for a Module / ModuleGroup fix #22

---
+++ init/04_Create_Module.sql
@@ -0,0 +1,55 @@
+
+---------------------------------------------------------------------
+
+-- Initializationscript for an example module
+
+---------------------------------------------------------------------
+
+INSERT INTO [woo].[ModuleGroup]
+           ([Icon]
+           ,[Name]
+           ,[Description])
+     VALUES
+           (null
+           ,'Test-Group'
+           ,'Contains all specific modules')
+GO
+INSERT INTO [woo].[Translation]
+           ([DefaultDescription])
+     VALUES
+           ('Test-Module')
+GO
+INSERT INTO [woo].[TranslationItem]
+           ([TranslationId]
+           ,[LanguageId]
+           ,[Description])
+     VALUES
+           ((SELECT TOP 1 Id FROM woo.Translation where DefaultDescription = 'Test-Module')
+           ,(SELECT TOP 1 Id FROM woo.[Language])
+           ,'Test-Module')
+GO
+INSERT INTO [woo].[Module]
+           ([Icon]
+           ,[Name]
+           ,[Description]
+           ,[Version]
+           ,[ModuleGroupId]
+           ,[LogicalId]
+           ,[Sequence]
+           ,[TranslationId])
+     VALUES
+           (null,
+           'My Test Module'
+           ,'Manages a specific function'
+           ,'0'
+           ,1
+           ,'TES'
+           ,0
+           ,(SELECT TOP 1 Id FROM woo.Translation where DefaultDescription = 'Test-Module'))
+GO
+
+
+
+
+
+
