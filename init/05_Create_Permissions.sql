59da8c3a1b1fb8fe8b5143df2a811613555af3ae
paroos <pa.roos@icloud.com>
2013-12-27 14:47:20 +0100

    Example script for creating crud permissions fix #23

---
+++ init/05_Create_Permissions.sql
@@ -0,0 +1,11 @@
+
+---------------------------------------------------------------------
+
+-- Initializationscript for CRUD permissions
+
+---------------------------------------------------------------------
+
+INSERT INTO woo.Permission VALUES ('FUNCTION', 'Basic function permission')
+INSERT INTO woo.Permission VALUES ('CREATE', 'Can create specific records')
+INSERT INTO woo.Permission VALUES ('EDIT', 'Can edit specific records')
+INSERT INTO woo.Permission VALUES ('DELETE', 'Can delete specific records')
