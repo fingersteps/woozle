c3fc393ce11709711e22e027e29c27a4a2372804
Andreas Schürmann <an.schuermann@gmail.com>
2014-01-10 16:16:15 +0100

    Removed reference to entity framework in woozle

--- src/woozle/Services/ExceptionCatcher.cs
+++ src/woozle/Services/ExceptionCatcher.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Data.Entity.Core;
 using PostSharp.Aspects;
 using ServiceStack.Logging;
-using Woozle.Common.Extensions;
 
 namespace Woozle.Services
 {
@@ -19,39 +17,8 @@ namespace Woozle.Services
             var msg = string.Format("{0} threw the following exception: {1}\n{2}",
                 args.Method.Name, args.Exception.Message, args.Exception.StackTrace);
             Logger.Error(msg);
-            throw ParseException(args);
-        }
-
-        private ServiceException ParseException(MethodExecutionArgs args)
-        {
-            var service = GetInstance<AbstractService>(args.Instance);
-            var message = "Folgender ungekannter Fehler ist aufgetreten: " + args.Exception.Message;
-            var exception = args.Exception;
-            var originalException = exception.GetOriginalException();
-            if (originalException is OptimisticConcurrencyException)
-            {
-                message =
-                    "Die Daten wurden in der Zwischenzeit von einem anderen Benutzer geändert. \nBitte öffnen Sie das aktuelle Fenster neu und wiederholen Sie Ihre Tätigkeit.";
-            }
-            else
-            {
-                message = exception.Message;
-            }
-            return new ServiceException(message);
-        }
-
-        /// <summary>
-        /// Gets the instance object in the desired type.
-        /// </summary>
-        private static T GetInstance<T>(object instanceObj)
-        {
-            if (instanceObj != null && instanceObj is T)
-            {
-                return (T)instanceObj;
-            }
-            var msg = string.Format(string.Format("No instance of type '{0}' was found.", typeof(T)));
-            Logger.Error(msg);
-            throw new SystemException(msg);
+            var message = "The following exception occured: " + args.Exception.Message;
+            throw new ServiceException(message);
         }
     }
 }
