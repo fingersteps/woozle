71d8442e51fadd3cdaa824b06e4f5b010dccf148
paroos <pa.roos@icloud.com>
2014-05-11 17:08:20 +0200

    IExternalEmailSystem integrated

---
+++ src/Woozle/ExternalSystem/IExternalEmailSystem.cs
@@ -0,0 +1,20 @@
+﻿using Woozle.Domain.ExternalSystem;
+
+namespace Woozle.ExternalSystem
+{
+    /// <summary>
+    /// Definition for an external system which is able to perform EMail related stuff.
+    /// </summary>
+    public interface IExternalEMailSystem : IExternalSystem
+    {
+        /// <summary>
+        /// Sends an EMail to a desired destination address
+        /// </summary>
+        /// <param name="fromName"> </param>
+        /// <param name="fromAddress"></param>
+        /// <param name="toAddress"></param>
+        /// <param name="subject"> </param>
+        /// <param name="text"></param>
+        bool SendEMail(string fromName, string fromAddress, string toAddress, string subject, string text);
+    }
+}
