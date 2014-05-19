417764151b91f9672cbcad7a18460c120a5dc459
paroos <pa.roos@icloud.com>
2014-05-19 22:13:03 +0200

    Some comments.

--- src/Woozle/Domain/Fields/ITextFieldLogic.cs
+++ src/Woozle/Domain/Fields/ITextFieldLogic.cs
@@ -5,7 +5,19 @@ namespace Woozle.Domain.Fields
 {
     public interface ITextFieldLogic
     {
+        /// <summary>
+        /// Gets all text fields of the specific mandator.
+        /// </summary>
+        /// <param name="session"><see cref="SessionData"/></param>
+        /// <returns>A list of <see cref="TextFieldSearchResult"/></returns>
         IEnumerable<TextFieldSearchResult> GetTextFields(SessionData session);
+
+        /// <summary>
+        /// Gets a specific text field.
+        /// </summary>
+        /// <param name="name">The name of the text field.</param>
+        /// <param name="session"><see cref="SessionData"/></param>
+        /// <returns>The <see cref="TextFieldSearchResult"/></returns>
         TextFieldSearchResult GetTextField(string name, SessionData session);
     }
 }
