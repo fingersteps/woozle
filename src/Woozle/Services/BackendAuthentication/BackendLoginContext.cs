efb5585428d92fd5836c27a9a9edd51915c5dcf5
paroos <pa.roos@icloud.com>
2014-07-06 12:52:38 +0200

    Fix #51

---
+++ src/Woozle/Services/BackendAuthentication/BackendLoginContext.cs
@@ -0,0 +1,16 @@
+﻿using ServiceStack.ServiceHost;
+using Woozle.Services.UserManagement;
+
+namespace Woozle.Services.BackendAuthentication
+{
+    [Route("/backendLoginContext", "GET")]
+    public class BackendLoginContext : IReturn<BackendLoginContextResult>
+    {
+    }
+
+    public class BackendLoginContextResult
+    {
+        public User User { get; set; }
+        public Mandator.Mandator Mandator { get; set; }
+    }
+}
---
+++ src/Woozle/Services/BackendAuthentication/BackendLoginContextService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ServiceStack.ServiceInterface;
+using Woozle.Services.Authentication;
+using Woozle.Services.Authority;
+using Woozle.Services.UserManagement;
+
+namespace Woozle.Services.BackendAuthentication
+{
+    [MandatorAuthenticate]
+    [RequiredRole(Roles.Administrator)]
+    public class BackendLoginContextService : AbstractService
+    {
+        /// <summary>
+        /// Gets the context of the user which is logged in to this Session (User and Mandator)
+        /// </summary>
+        /// <param name="requestDto"></param>
+        /// <returns></returns>
+        [ExceptionCatcher]
+        public BackendLoginContextResult Get(BackendLoginContext requestDto)
+        {
+            var serviceUser = Mapper.Map<Model.User, User>(Session.SessionData.User);
+            var serviceMandator = Mapper.Map<Model.Mandator, Mandator.Mandator>(Session.SessionData.Mandator);
+            var result = new BackendLoginContextResult() { User = serviceUser, Mandator = serviceMandator };
+            return result;
+        }
+    }
+}
